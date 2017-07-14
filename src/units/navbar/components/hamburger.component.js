4bc450c180fa37004d390b42c2cc1bb944f112de
aconfee <adamestela@gmail.com>
2017-07-13 23:00:13 -0500

    Hamburger added. Made logo responsive. Made links responsive -- although no open mobile header available yet.

--- src/units/navbar/components/hamburger.component.js
+++ src/units/navbar/components/hamburger.component.js
@@ -6,23 +6,35 @@ class Hamburger extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { isOpen: false };
+    this.state = { isOpen: false, defaultState: true };
   }
 
   handleClick = () => {
-    this.setState( prevState =>  ({ isOpen: !prevState.isOpen }));
+    this.setState( prevState =>  ({ isOpen: !prevState.isOpen, defaultState: false }));
   }
 
-  animate = () => {
-    return this.state.isOpen ? "animate" : "";
+  animateHamburger = () => {
+    if(this.state.defaultState === true) return "";
+
+    return this.state.isOpen ? "animate" : "hide";
+  }
+
+  animateCross = () => {
+    if(this.state.defaultState === true) return "hide";
+
+    return this.state.isOpen ? "hide" : "animate";
   }
 
   render() {
     return (
       <div className="hamburger" onClick={ this.handleClick }>
-        <div id="top" className={ "hamburger-line " + this.animate() }></div>
-        <div id="middle" className={ "hamburger-line " + this.animate() }></div>
-        <div id="bottom" className={ "hamburger-line " + this.animate() }></div>
+        <div id="top-burger" className={ "line top " + this.animateHamburger() }></div>
+        <div id="middle-burger" className={ "line middle " + this.animateHamburger() }></div>
+        <div id="bottom-burger" className={ "line bottom " + this.animateHamburger() }></div>
+
+        <div id="top-cross" className={ "line top " + this.animateCross() }></div>
+        <div id="middle-cross" className={ "line middle " + this.animateCross() }></div>
+        <div id="bottom-cross" className={ "line bottom " + this.animateCross() }></div>
       </div>
     );
   }
