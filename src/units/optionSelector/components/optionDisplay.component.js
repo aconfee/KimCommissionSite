031c58963024b56a8663da6fb5aa1f6cb4cc8e9d
aconfee <adamestela@gmail.com>
2017-07-23 11:46:05 -0500

    flip is in as-is

--- src/units/optionSelector/components/optionDisplay.component.js
+++ src/units/optionSelector/components/optionDisplay.component.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './optionDisplay.component.css';
+
+PropTypes.OptionDisplay = {
+  images: PropTypes.arrayOf(PropTypes.string),
+  activeIndex: PropTypes.number
+}
 
 class OptionDisplay extends Component {
 
+  constructor(props) {
+    super(props);
+
+    const {
+      images,
+      activeIndex
+    } = this.props;
+
+    this.state = {
+      frontImageUrl: images[activeIndex],
+      backImageUrl: images[activeIndex + 1]
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.playAnimation();
+
+    setTimeout(function() {
+      this.resetAnimtion();
+    }.bind(this),
+    3000);
+  }
+
+  playAnimation = () => {
+    console.log('playing animation');
+  }
+
+  resetAnimtion = () => {
+    console.log('reseting animation');
+  }
+
   render() {
+
+    // This will all change. Will only use state.frontImageUrl and state.backImageUrl
+    // These states are modified in constructor and willReceiveProps/playanimation.
+    // shouldn't have to derive anything here.
     return (
       <div className="option-display-container">
+        <div className='flipable'>
+          <div className='front'>
+            <img className="display-image" src={ this.state.frontImageUrl } alt="level of detail display" title="level of detail display" />
+          </div>
+          <div className='back'>
+            <img className="display-image" src={ this.state.backImageUrl } alt="level of detail display" title="level of detail display" />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default OptionDisplay;
--- src/units/optionSelector/components/optionSelectorContainer.component.js
+++ src/units/optionSelector/components/optionSelectorContainer.component.js
@@ -4,10 +4,37 @@ import OptionButtons from './optionButtons.component.js';
 import OptionDisplay from './optionDisplay.component.js';
 
 class OptionSelectorContainer extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: 0
+    }
+
+    // TESTING ONLY
+    setTimeout(function() {
+      this.setState({ activeIndex: 1 });
+    }.bind(this),
+    3000);
+  }
+
+   getImages = () => {
+    // TODO: This will become props that are passed in to the selector container.
+    // This will drive how many buttons appear as well. This basically controls
+    // how this entire thing will render / behave.
+    return [
+      "./images/logo.jpg",
+      "./images/details-cover.jpg"
+    ];
+  }
+
   render() {
     return (
       <div className="option-selector-container">
-        <OptionDisplay />
+
+        <OptionDisplay images={ this.getImages() } activeIndex={ this.state.activeIndex }/>
+
         <p className="font-size-small font-color-dark description-text">How much detail do you want?</p>
         <span className="font-size-small font-color-light buttons-before">$</span>
         <OptionButtons />
