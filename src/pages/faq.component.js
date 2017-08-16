d10198c30c3344dc417dae95719f66e120b35a38
aconfee <adamestela@gmail.com>
2017-08-15 23:20:37 -0500

    faq page

--- src/pages/faq.component.js
+++ src/pages/faq.component.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './faq.component.css';
 
 class Faq extends Component {
   render() {
     return(
-      <h1>F.A.Q. Page</h1>
+      <div className="faq-container">
+        <div className="faq-image">
+          <img src="./images/details-cover.jpg" alt="kimbyarting mermaid art" title="kimby arting mermaid art" />
+        </div>
+        <div className="faq-entries">
+          <p className="header">What is your commission process?</p>
+          <p className="text">Blah blah blah. Things.</p>
+          <p className="header">Can I cancel a commission?</p>
+          <p className="text">Hell nah. I cut you. </p>
+        </div>
+      </div>
     );
   }
 }
